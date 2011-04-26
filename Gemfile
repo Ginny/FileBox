7ff464ab8e6326c5b1f458110a24e4f0da1d72e2
Jan Gnip <gnip.jan@gmail.com>
2011-04-26 16:40:20 +0200

    second commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'ruby-mysql'
+
+# Gem for autentification
+gem 'devise', :git => 'http://github.com/plataformatec/devise.git'
+
+# Handy generators for layout, scaffolding, etc.
+gem 'nifty-generators', :group => :development
+
+# For file uploads
+gem 'paperclip', '~> 2.3'
+
+# For facebook login
+gem 'oauth2'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Filebox::Application.config.secret_token = '5e721839e529493f37432222788a366f47fa38bfc5933978713625d4951934c23f73302a219c0b9a50178556bec1cf60650347a900b88e7b2181ddb6ea4ecaea'
