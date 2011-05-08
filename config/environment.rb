07fc7822d7c2025f6b9f836a5da47ce9dfa5a1da
Jan Gnip <gnip.jan@gmail.com>
2011-05-08 12:37:03 +0200

    added styles and homepage

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+#Formatting DateTime to look like "20/01/2011 10:28PM"  
+Time::DATE_FORMATS[:default] = "%d/%m/%Y %l:%M%p"  
+    
 # Initialize the rails application
 Filebox::Application.initialize!
